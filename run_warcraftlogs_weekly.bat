e1918b33a3c52b42c6e6f0da9666dad5bb84852a
Karl <karl.tengstrom@gmailcom>
2025-09-22 13:43:52 +0200

    Removed files from remote repository

--- run_warcraftlogs_weekly.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "C:\Users\karlt\Documents\ec_utbildning\python\warlog_project"
-"C:\Users\karlt\Documents\ec_utbildning\python\.venv\Scripts\python.exe" "warcraftlogs_get_data.py"
-pause
-
